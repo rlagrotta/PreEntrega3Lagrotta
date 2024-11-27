dc8dc07f99fa5f3c1d9309f3de521e82062ed901
rlagrotta <roque@rocketpowerstudios.com>
2024-11-27 15:38:51 -0500

    PreEntrega 3

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,11 +1,15 @@
 import { createContext, useState, useContext } from "react";
+
 export const CartContext = createContext();
 
-export const CartProvider = ({ children }) => {
+export const CartProvider = ({children}) => {
     const [cart, setCart] = useState([]);
+    const addItem = (item,cantidad)=>{
+        setCart([...cart,{...item, qty: cantidad}])
+    }
 
     return (
-        <CartContext.Provider value={{ cart}}>
+        <CartContext.Provider value={{cart,value}}>
             {children}
         </CartContext.Provider>
     );
